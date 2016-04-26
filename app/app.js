49f3437620724fb3d354525f898339da16a7c7dc
Antoine Chabert <chabert.antoine@gmail.com>
2016-04-26 15:38:30 +0200

    Add loading progress bar and animation in angular

--- app/app.js
+++ app/app.js
@@ -3,10 +3,13 @@
 // Declare app level module which depends on views, and components
 angular.module('lyonRewards', [
   'ngRoute',
+  'ngAnimate',
+  'angular-loading-bar',
   'lyonRewards.view1',
   'lyonRewards.view2',
   'lyonRewards.version'
 ]).
-config(['$routeProvider', function($routeProvider) {
+config(['$routeProvider', 'cfpLoadingBarProvider', function($routeProvider, cfpLoadingBarProvider) {
+  cfpLoadingBarProvider.latencyThreshold = 0;
   $routeProvider.otherwise({redirectTo: '/view1'});
 }]);
