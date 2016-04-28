6ba1268cb3326667286be558d6701a6a220f714d
Antoine Chabert <chabert.antoine@gmail.com>
2016-04-28 17:22:07 +0200

    Add sub pages in dashboard

--- app/view/page/dashboard/dashboard.js
+++ app/view/page/dashboard/dashboard.js
@@ -2,19 +2,33 @@
 
 var appPageDashboard = angular.module('lyonRewards.dashboard', ['ngRoute']);
 
+var checkUserLogin = function ($q, $rootScope, $location) {
+  if ($rootScope.isLogin) {
+    return true;
+  } else {
+    $location.path('/login');
+  }
+};
+
 appPageDashboard.config(['$routeProvider', function($routeProvider) {
   $routeProvider.when('/dashboard', {
-    templateUrl: 'view/page/dashboard/dashboard.html',
-    controller: 'DashboardCtrl' /*, TODO Uncomment in production
+    templateUrl: 'view/page/dashboard/page/dashboard.html',
+    controller: 'DashboardCtrl',
     resolve: {
-      factory: function ($q, $rootScope, $location) {
-        if ($rootScope.isLogin) {
-          return true;
-        } else {
-          $location.path('/login');
-        }
-      }
-    }*/
+      factory: checkUserLogin
+    }
+  }).when('/dashboard/history', {
+    templateUrl: 'view/page/dashboard/page/history.html',
+    controller: 'DashboardHistoryCtrl',
+    resolve: {
+      factory: checkUserLogin
+    }
+  }).when('/dashboard/settings', {
+    templateUrl: 'view/page/dashboard/page/settings.html',
+    controller: 'DashboardSettingsCtrl',
+    resolve: {
+      factory: checkUserLogin
+    }
   }).otherwise({ redirectTo: '/' });
 
 }]);
@@ -22,3 +36,12 @@ appPageDashboard.config(['$routeProvider', function($routeProvider) {
 appPageDashboard.controller('DashboardCtrl', function($scope, $http) {
 
 });
+
+appPageDashboard.controller('DashboardHistoryCtrl', function($scope, $http) {
+
+});
+
+appPageDashboard.controller('DashboardSettingsCtrl', function($scope, $http) {
+
+});
+
