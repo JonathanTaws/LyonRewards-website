890f2df876aabf6cec4621db9f7378148e912f26
Antoine Chabert <chabert.antoine@gmail.com>
2016-04-29 13:10:44 +0200

    Add first chart

--- app/view/page/dashboard/dashboard.js
+++ app/view/page/dashboard/dashboard.js
@@ -48,3 +48,15 @@ appPageDashboard.controller('DashboardSettingsCtrl', function($scope, $http) {
 
 });
 
+appPageDashboard.controller("PointsEarnedCtrl", function ($scope) {
+
+  $scope.labels = ["January", "February", "March", "April", "May", "June", "July"];
+  $scope.series = ['Series A', 'Series B'];
+  $scope.data = [
+    [65, 59, 80, 81, 56, 55, 40]
+  ];
+  $scope.onClick = function (points, evt) {
+    console.log(points, evt);
+  };
+});
+
