7d5de14fb99292f59daee6d60bcae0bce95cc956
Antoine Chabert <chabert.antoine@gmail.com>
2016-04-30 15:15:31 +0200

    Fix angular moment with beta version - Add start and end date in all events in home page - Add events not ended score in top

--- app/view/page/home/home.js
+++ app/view/page/home/home.js
@@ -3,7 +3,8 @@
 var appPageHome = angular.module('lyonRewards.home', [
   'ngRoute',
   'angularMoment',
-  'uiGmapgoogle-maps'
+  'uiGmapgoogle-maps',
+  'angular.filter'
 ]);
 
 appPageHome.config(function($routeProvider, uiGmapGoogleMapApiProvider) {
@@ -45,14 +46,17 @@ appPageHome.controller('HomeCtrl', function($scope, $http) {
 });
 
 appPageHome.controller('EventCtrl', function($scope, $log, $timeout) {
+
   $scope.isCollapsed = true;
   $scope.isVisible = false;
+
   $scope.toggle = function() {
     $timeout(function() {
       $scope.isVisible = !$scope.isVisible;
     }, 500);
     $scope.isCollapsed = !$scope.isCollapsed;
-  }
+  };
+
   $scope.map = {
     center: {
       latitude: $scope.event.latitude,
@@ -60,4 +64,40 @@ appPageHome.controller('EventCtrl', function($scope, $log, $timeout) {
     },
     zoom: 11
   };
+
+});
+
+appPageHome.controller('EventsInfoCtrl', function($scope, $log, $timeout){
+
+  $scope.currentEvents = [];
+
+  $scope.$watch('events', function () {
+    var now = new Date();
+    angular.forEach($scope.events, function(value) {
+      if (new Date(value.end_date) >= now) {
+        $scope.currentEvents.push(value);
+      }
+    });
+  });
+
+  $scope.isCollapsed = true;
+  $scope.isVisible = false;
+
+  $scope.toggle = function() {
+    $timeout(function() {
+      $scope.isVisible = !$scope.isVisible;
+    }, 500);
+    $scope.isCollapsed = !$scope.isCollapsed;
+  };
+
+  // Maps center on Lyon
+  $scope.map = {
+    center: {
+      latitude: 45.7663357,
+      longitude: 4.8559309
+    },
+    zoom: 13
+  };
+
+
 });
