5354d4aa77be60d84ba92d1bba71396e4e145021
Antoine Chabert <chabert.antoine@gmail.com>
2016-05-02 08:34:17 +0200

    Add ranking with API

--- app/view/page/ranking/ranking.js
+++ app/view/page/ranking/ranking.js
@@ -9,6 +9,26 @@ appPageRanking.config(['$routeProvider', function($routeProvider) {
   });
 }]);
 
-appPageRanking.controller('RankingCtrl', [function() {
+appPageRanking.controller('RankingCtrl', function($scope, $http, $log) {
 
-}]);
+  $scope.usersRanking = [];
+  var loaderRankingElt = jQuery('.ranking-page .loader-ranking');
+  var rankingElt = jQuery('.ranking-page .ranking');
+  var filtersElt = jQuery('.ranking-page .filters');
+  rankingElt.fadeOut(0);
+  filtersElt.fadeOut(0);
+  var displayRanking = function() {
+    loaderRankingElt.hide();
+    rankingElt.fadeIn(500);
+    filtersElt.fadeIn(500);
+  };
+  var successRanking = function(response) {
+    $log.debug(response);
+    $scope.usersRanking = response.data;
+    displayRanking();
+  };
+  var errorRanking = function(response) {
+    $log.debug(response);
+    displayRanking();
+  };
+});
